d0b19883b6f438cca412f891d7d000c91c3ed363
killian05000 <killianwolfger@hotmail.fr>
2019-04-03 00:38:11 +0200

    no need for reader

--- Model.py
+++ Model.py
@@ -4,18 +4,12 @@ from sklearn.metrics import accuracy_score
 import os
 
 import sys
-sys.path.insert(0,'..')
-import Reader as re
-sys.path.insert(0,'')
 
 folder = 'Models'
 
 def load_Model(f,data):
     loadeded_model=pickle.load(open(folder+'/'+f,'rb'))
-    print("dans load model taille data :",len(data))
-    print(data.shape)
     result=loadeded_model.predict(data)
-    print(result.shape)
     return result
 
 def save_Model(filename,model):
