2beae0c31599f94712381a6cb74900171f5337bd
killian05000 <killianwolfger@hotmail.fr>
2019-04-03 00:39:41 +0200

    predict working fine, weaker results but more trustworthy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import argparse
-import Reader as re
 import Images.DataGetter as dg
 import time
 import OutputJson as jout
@@ -56,25 +55,18 @@ if(pathTrain is not None):
 
         listeAlgo = Algorithm.Algos()
         data_I, target_I = DataEntry.getDataImageVec(pathTrain)
-        data_H, target_H = DataEntry.getDataHistogramme(pathTrain)
         data_S, target_S = DataEntry.getFastDataSobel(pathTrain)
+        data_H, target_H = DataEntry.getDataHistogramme(pathTrain)
+
 
         ########### Algos lancé sur un vecteur d'image
         listeAlgo.setDataTarget(data_I, target_I)
         tmp_bayes_image = listeAlgo.Bayes('Bayes_ImageVec.sav')
         tmp_ada_image = listeAlgo.Ada_boost('AdaBoost_ImageVec.sav')
-        tmp_svm_image = listeAlgo.svmImgvec('Svm_Imagevec.sav')
-
+        tmp_svm_image = listeAlgo.svmImgvec('Svm_ImageVec.sav')
         tmp_accuracy_bayes_i = accuracy_score(listeAlgo.target_test, tmp_bayes_image)
-        #print(tmp_accuracy_bayes_i)
         tmp_accuracy_ada_i = accuracy_score(listeAlgo.target_test,tmp_ada_image)
-        #print(tmp_accuracy_ada_i)
         tmp_accuracy_svm_i = accuracy_score(listeAlgo.target_test, tmp_svm_image)
-        #print(tmp_accuracy_svm_i)
-
-
-
-
 
 
         ########### Algos lancé sur un vecteur de contour d'image
@@ -82,13 +74,9 @@ if(pathTrain is not None):
         tmp_bayes_sobel = listeAlgo.Bayes('Bayes_Sobel.sav')
         tmp_ada_sobel = listeAlgo.Ada_boost('AdaBoost_Sobel.sav')
         tmp_svm_sobel = listeAlgo.svmImgvec('Svm_Sobel.sav')
-
         tmp_accuracy_bayes_s = accuracy_score(listeAlgo.target_test, tmp_bayes_sobel)
-        #print(tmp_accuracy_bayes_s)
         tmp_accuracy_ada_s = accuracy_score(listeAlgo.target_test, tmp_ada_sobel)
-        #print(tmp_accuracy_ada_s)
         tmp_accuracy_svm_s = accuracy_score(listeAlgo.target_test, tmp_svm_sobel)
-        #print(tmp_accuracy_svm_s)
 
 
         ########### Algos lancé sur un histogramme de couleur
@@ -96,36 +84,10 @@ if(pathTrain is not None):
         tmp_bayes_histo = listeAlgo.Bayes('Bayes_Histo.sav')
         tmp_ada_histo = listeAlgo.Ada_boost('AdaBoost_Histo.sav')
         tmp_svm_histo = listeAlgo.svmImgvec('Svm_Histo.sav')
-
         tmp_accuracy_bayes_h = accuracy_score(listeAlgo.target_test, tmp_bayes_histo)
-        #print(tmp_accuracy_bayes_h)
         tmp_accuracy_ada_h = accuracy_score(listeAlgo.target_test, tmp_ada_histo)
-        #print(tmp_accuracy_ada_h)
         tmp_accuracy_svm_h = accuracy_score(listeAlgo.target_test, tmp_svm_histo)
-        #print(tmp_accuracy_svm_h)
 
-        vectorH=[tmp_bayes_histo*tmp_accuracy_bayes_h,
-               tmp_ada_histo*tmp_accuracy_ada_h,
-               tmp_svm_histo*tmp_accuracy_svm_h]
-
-
-        '''
-        vector = [tmp_bayes_image*(tmp_accuracy_bayes_i),
-                  tmp_ada_image*(tmp_accuracy_ada_i),
-                  tmp_svm_image*(tmp_accuracy_svm_i),
-                  tmp_bayes_histo*(tmp_accuracy_bayes_h),
-                  tmp_ada_histo*(tmp_accuracy_ada_h),
-                  tmp_svm_histo*(tmp_accuracy_svm_h),
-                  tmp_bayes_sobel*(tmp_accuracy_bayes_s),
-                  tmp_ada_sobel*(tmp_accuracy_ada_s),
-                  tmp_svm_sobel*(tmp_accuracy_svm_s),]
-        '''
-
-        print('\n')
-        #for i in range(len(vector)):
-            #print(vector[i],'\n')
-        voteResult=Moyenne.Votes(vectorH)
-        accuracy+=accuracy_score(listeAlgo.target_test, voteResult)
 
         print("Bayes Image : ",tmp_accuracy_bayes_i)
         print("Ada Image :",tmp_accuracy_ada_i)
@@ -137,17 +99,6 @@ if(pathTrain is not None):
         print("Ada Sobel :",tmp_accuracy_ada_s)
         print("Svm Sobel :",tmp_accuracy_svm_s)
 
-        '''
-        print("Bayes Image : ",tmp_bayes_image)
-        print("Ada Image :",tmp_ada_image)
-        print("Svm Image :",tmp_svm_image)
-        print("Bayes Histo :",tmp_bayes_histo)
-        print("Ada Histo :",tmp_ada_histo)
-        print("Svm Histo :",tmp_svm_histo)
-        print("Bayes Sobel :",tmp_bayes_sobel)
-        print("Ada Sobel :",tmp_ada_sobel)
-        print("Svm Sobel :",tmp_svm_sobel)
-        '''
         accuracy_bayes_Img += tmp_accuracy_bayes_i
         accuracy_ada_Img += tmp_accuracy_ada_i
         accuracy_svm_Img += tmp_accuracy_svm_i
@@ -157,18 +108,7 @@ if(pathTrain is not None):
         accuracy_bayes_Sobel += tmp_accuracy_bayes_s
         accuracy_ada_Sobel += tmp_accuracy_ada_s
         accuracy_svm_Sobel += tmp_accuracy_svm_s
-        '''
-        accuracy_bayes_Img += tmp_bayes_image
-        accuracy_ada_Img += tmp_ada_image
-        accuracy_svm_Img += tmp_svm_image
-        accuracy_bayes_Histo += tmp_bayes_histo
-        accuracy_ada_Histo += tmp_ada_histo
-        accuracy_svm_Histo += tmp_svm_histo
-        accuracy_bayes_Sobel += tmp_bayes_sobel
-        accuracy_ada_Sobel += tmp_ada_sobel
-        accuracy_svm_Sobel += tmp_svm_sobel
-        '''
-        print("Resultat du vote ",nbIter,": ",accuracy_score(listeAlgo.target_test, voteResult),'\n')
+
         time2=time.clock()
         print("Execution Time ",i," : ",round((time2-time1),4),'sec\n')
         nbIter+=1
@@ -194,43 +134,32 @@ if(pathPredict is not None):
     print("--Predict start--")
 
     dataHisto,fileNames = DataEntry.dataHistogrammePredict(pathPredict)
-    print(dataHisto.shape)
     resultBayesHisto=Model.load_Model('Bayes_Histo.sav', dataHisto)
     resultAdaHisto=Model.load_Model('AdaBoost_Histo.sav', dataHisto)
     resultSvmHisto=Model.load_Model('Svm_Histo.sav', dataHisto)
-    print("load Histo done")
-    '''
+    print("----load Histo Models done----")
+
 
     dataImageVec,fileNames = DataEntry.dataImageVecPredict(pathPredict)
-    print(dataImageVec.shape)
     resultBayesImageVec=Model.load_Model('Bayes_ImageVec.sav',dataImageVec)
     resultAdaImageVec=Model.load_Model('AdaBoost_ImageVec.sav',dataImageVec)
     resultSvmImageVec=Model.load_Model('Svm_ImageVec.sav',dataImageVec)
-    print("load Image done")
+    print("----load Image Models done----")
 
 
     dataSobel,fileNames = DataEntry.dataSobelPredict(pathPredict)
     resultBayesSobel=Model.load_Model('Bayes_Sobel.sav',dataSobel)
     resultAdaSobel=Model.load_Model('AdaBoost_Sobel.sav',dataSobel)
     resultSvmSobel=Model.load_Model('Svm_Sobel.sav',dataSobel)
-    print("load sobel done")
-    '''
+    print("----load Sobel Models done----")
 
-
-
-
-
-    '''
     vector=[resultBayesImageVec,resultAdaImageVec, resultSvmImageVec,
             resultBayesHisto, resultAdaHisto, resultSvmHisto,
             resultBayesSobel, resultAdaSobel, resultSvmSobel]
-            '''
 
-    vector=[resultBayesHisto, resultAdaHisto, resultSvmHisto]
     voteResult=Moyenne.Votes(vector)
-    for i in range(len(vector)):
-        print(vector[i],'\n')
-
-    print(voteResult)
+    #for i in range(len(vector)):
+        #print(vector[i],'\n')
+    #print(voteResult)
 
     Json.outPutJson(fileNames, pathPredict, voteResult)
